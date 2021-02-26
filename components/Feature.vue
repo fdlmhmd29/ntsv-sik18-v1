6a2c0da82b3b455d5c85df0b2d8808253c84463e
Fadhil Muhammad <fadhilmhmd29@gmail.com>
2021-02-26 20:24:59 +0700

    Fourth Commit

--- components/Feature.vue
+++ components/Feature.vue
@@ -1,6 +1,8 @@
 <template>
-  <div v-editable="blok" class="py-2">
-    <h1 class="text-lg">{{ blok.name }}</h1>
+  <div v-editable="blok" class="py-2 text-center">
+    <img class="mx-auto" :src="blok.icon" />
+    <h1 class="text-lg text-center">{{ blok.name }}</h1>
+    <p class="text-gray-600">{{ blok.description }}</p>
   </div>
 </template>
 
